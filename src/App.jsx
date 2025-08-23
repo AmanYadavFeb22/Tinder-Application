6df94ebbbf47ff55b6ca3975023506285fcd6869
AmanYadavFeb22 <ay415282@gmail.com>
2025-08-23 19:32:50 +0530

    Create Routing Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 
 const App = () => {
   return (
     <>
-      <h1>Hello World!!!!</h1>
+   <BrowserRouter basename="/">
+   <Routes>
+    <Route path="/" element={<Body/>} >
+    <Route path="/login" element={<Login/>} />
+    <Route path="/profile" element={<Profile/>} />
+
+
+
+    </Route>
+
+
+
+
+   </Routes>
+   
+   
+   </BrowserRouter>
+
+   
     </>
   );
 };
